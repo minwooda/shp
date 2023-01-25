23a88615cca73c305ffbc884bb7395fae0bce86b
minwooda <pmw5090@naver.com>
2023-01-25 16:32:42 +0900

    회원가입 완료

--- newshp/src/main/java/com/newshp/newshp/controller/MainController.java
+++ newshp/src/main/java/com/newshp/newshp/controller/MainController.java
@@ -1,13 +1,68 @@
 package com.newshp.newshp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.newshp.newshp.model.User;
+import com.newshp.newshp.repository.UserRepository;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     
-    @RequestMapping(value = "/index")
+    @Autowired
+    UserRepository userRepository;
+    
+    @GetMapping({"","/"})
     public String index(){
         return "index";
     }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "login/loginForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+
+        userRepository.save(user); //시큐리티로 로그인 할 수 없음 , 패스워드가 암호화가 안되어있으므로
+
+
+        return "redirect:login/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "join/joinForm";
+    }
+
+
 }
